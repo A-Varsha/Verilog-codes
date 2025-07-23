dd613b1c859c6aaa9e85c6a65c4d33837a4015ed
A-Varsha <146081605+A-Varsha@users.noreply.github.com>
2025-07-23 17:49:35 +0530

    Add files via upload

---
+++ Day_01/parity_bit_tb.v
@@ -0,0 +1,53 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 23.07.2025 09:41:38
+// Design Name: 
+// Module Name: parity_bit_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module parity_bit_tb();
+reg [8:0]x;
+wire out;
+parity_bit dut(.x(x),
+.out(out),
+.cp(cp)
+);
+initial 
+begin
+x=9'b000000100;
+#10;
+x=9'b000101011;
+#10;
+x=9'b000000000;
+#10;
+x=9'b101010001;
+#10;
+x=9'b101010101;
+#10;
+x=9'b110010100;
+#10;
+x=9'b000000001;
+#10;
+x=9'b001010101;
+#10;
+x=9'b100000000;
+#10;
+
+$finish;
+end
+endmodule
